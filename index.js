8d119df20fcd2ca389497abb06c55ef6af74af02
Shelby Morris <shelbynicole.morris@gmail.com>
2023-03-05 16:46:26 -0700

    Couldn't reproduce this.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; //JUST AN ARRAY! 
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = function(fare){
+    return fare * 2
+}
+
+const fareTripler = function(fare){
+    return fare * 3
+}
+
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+    
+}
